612f4ee1ada73303bb827dec49dcca61e0452774
Vlastiss <120052173+Vlastiss@users.noreply.github.com>
2024-02-05 20:12:58 +0000

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,19 @@
 // Input: list1 = [], list2 = [0]
 // Output: [0]
 
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+
+var mergeTwoLists = function(list1, list2) {
+    
+};

db2e125da71798398dd9dabfc36e466542f4f0a2
Vlastiss <120052173+Vlastiss@users.noreply.github.com>
2024-02-05 20:32:43 +0000

    Update script.js

--- script.js
+++ script.js
@@ -23,9 +23,7 @@
  */
 
 var mergeTwoLists = function(list1, list2) {
-    console.log(list1);
-    var newArray = list1.concat(list2);
-    newArray.sort();
+    
 };
 
-mergeTwoLists([1,2,4], [1,3,4]);
+console.log(mergeTwoLists([1,2,4], [1,3,4]));
